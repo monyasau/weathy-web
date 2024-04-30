bb222c15cac508abe74481bc18650ca35085637c
Olajide Olanrewaju <Olajide.m.olanrewaju@gmail.com>
2024-04-30 21:44:42 +0100

    update

--- App.js
+++ App.js
@@ -1,16 +1,34 @@
+import { StatusBar } from "expo-status-bar";
+import { View } from "react-native";
+import "expo-dev-client";
+import Weathy from "./components/Weathy";
+import {
+  BannerAd,
+  BannerAdSize,
+  TestIds,
+  RewardedAd
+} from "react-native-google-mobile-ads";
+import TopNav from "./components/TopNav";
 
-import { StatusBar } from 'expo-status-bar';
-import { Text, View, SafeAreaView } from 'react-native';
-import "expo-dev-client"
 export default function App() {
+  const BannerAdUnitId = "ca-app-pub-2165582252373154/5631183082";
+  const RewardedAdUnitId = __DEV__ ? TestIds.REWARDED : 'ca-app-pub-2165582252373154/4559496768';
+
   return (
-
-    
-<View className="flex-1 items-center justify-center bg-white">
-        <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-
-      
-    </View>
+    <>
+      <View className="flex-1 items-center justify-center pt-8 bg-white">
+        <StatusBar style="auto" />
+        <TopNav/>
+        <Weathy />
+      </View>
+      <BannerAd
+      className=""
+        unitId={BannerAdUnitId}
+        size={BannerAdSize.ANCHORED_ADAPTIVE_BANNER}
+        requestOptions={{
+          requestNonPersonalizedAdsOnly: false,
+        }}
+      />
+    </>
   );
 }
